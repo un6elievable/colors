4d1021b0f4568399c78b724976752d7758259fb5
Евгений <110891298+un6elievable@users.noreply.github.com>
2022-08-09 12:53:38 +0300

    updating js file

--- script.js
+++ script.js
@@ -13,26 +13,7 @@ function changeColor(){
   document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
 }
 
-
-// function changeGradientColor {
-//   let  grdhex_numbers = ["0","1","2","3","4","5","6","7","8",
-//   "9","A","B","C","D","E","F",];
-//   let hexcode1 = '';
-//   let hexcode2 = '';
-//   let random_ind = 0;
-
-//   for (let i = 0; i<6;i++){
-//     random_ind = Math.floor(Math.random() * grdhex_numbers.length);
-//     hexcode1 += grdhex_numbers[random_ind];
-//     random_ind = Math.floor(Math.random() * grdhex_numbers.length);
-//     hexcode2 += grdhex_numbers[random_ind];    
-//   }
-
-//   document.getElementById("hexcode1").innerHTML = grdhexcode;
-//   document.getElementById("hexcode2").innerHTML = grdhexcode;
-//   document.getElementsByTagName("body")[0].style.background = "#" + grdhexcode;
-
-// }.
+//adding gradient color picker
 
 document.getElementById("submit").addEventListener("click", () => {
   var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
